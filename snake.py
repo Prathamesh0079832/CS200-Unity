b4a12ff507fdb50bc60fa685ac882daeb3a35e2f
bharath <nbsbharath@iitbbilai.ac.in>
2022-03-09 00:21:56 +0530

    initializing functions, board and snake

---
+++ snake.py
@@ -0,0 +1,75 @@
+from tkinter import *
+import numpy as np
+from PIL import ImageTk,Image
+
+
+size_of_board = 600
+rows = 10
+cols = 10
+DELAY = 100
+snake_initial_length = 3
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 2
+RED_COLOR = "#EE4035"
+BLUE_COLOR = "#0492CF"
+Green_color = "#7BC043"
+
+BLUE_COLOR_LIGHT = '#67B0CF'
+RED_COLOR_LIGHT = '#EE7E77'
+
+
+class SnakeAndApple:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Snake-and-Apple")
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        # Input from user in form of clicks and keyboard
+        self.window.bind("<Key>", self.key_input)
+        self.window.bind("<Button-1>", self.mouse_input)
+        self.play_again()
+        self.begin = False
+
+# Initialized Functions
+
+
+    def initialize_board(self):
+        self.board = []
+        self.apple_obj = []
+        self.old_apple_cell = []
+
+        for i in range(rows):
+            for j in range(cols):
+                self.board.append((i, j))
+
+        for i in range(rows):
+            self.canvas.create_line(
+                i * size_of_board / rows, 0, i * size_of_board / rows, size_of_board,
+            )
+
+        for i in range(cols):
+            self.canvas.create_line(
+                0, i * size_of_board / cols, size_of_board, i * size_of_board / cols,
+            )
+            
+            
+# # Initialized Board
+
+
+    def initialize_snake(self):
+        self.snake = []
+        self.crashed = False
+        self.snake_heading = "Right"
+        self.last_key = self.snake_heading
+        self.forbidden_actions = {}
+        self.forbidden_actions["Right"] = "Left"
+        self.forbidden_actions["Left"] = "Right"
+        self.forbidden_actions["Up"] = "Down"
+        self.forbidden_actions["Down"] = "Up"
+        self.snake_objects = []
+        for i in range(snake_initial_length):
+            self.snake.append((i, 0))
+            
+            
+# Initialized snake
