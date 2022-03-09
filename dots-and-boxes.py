67da9c0f1d1e01686f05d838c6b32409ed19bb06
Prat <rprathamesh@iitbhilai.ac.in>
2022-03-09 10:34:41 +0530

    Added the initialization functions

--- dots-and-boxes.py
+++ dots-and-boxes.py
@@ -17,3 +17,28 @@ distance_between_dots=size_of_board/(number_of_dots)
 
 class Dots_and_Boxes():
     def __init__(self):
+        self.window = Tk()
+        self.window.title('Dots_and_Boxes')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        self.window.bind('<Button-1>', self.click)
+        self.player1_starts = True
+        self.refresh_board()
+        self.play_again()
+
+    def play_again(self):
+        self.refresh_board()
+        self.board_status = np.zeros(shape=(number_of_dots - 1, number_of_dots - 1))
+        self.row_status = np.zeros(shape=(number_of_dots, number_of_dots - 1))
+        self.col_status = np.zeros(shape=(number_of_dots - 1, number_of_dots))
+
+        self.player1_starts = not self.player1_starts
+        self.player1_turn = not self.player1_starts
+        self.reset_board = False
+        self.turntext_handle = []
+        self.already_marked_boxes = []
+        self.display_turn_text()
+
+    def mainloop(self):
+        self.window.mainloop()
+
