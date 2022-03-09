42a7f26247303789913b77659020650a4184c7d9
Prat <rprathamesh@iitbhilai.ac.in>
2022-03-09 22:49:15 +0530

    Made some small changes in variable names and added the execution of the main function

--- dots-and-boxes.py
+++ dots-and-boxes.py
@@ -5,12 +5,12 @@ size_of_board=600
 number_of_dots=6
 symbol_size=(size_of_board/3-size_of_board/8)/2
 symbol_thickness=50
-dot_colour='#5eab20'
-player1_colour='#158abd'
-player1_colour_light='#7fbad4'
-player2_colour='#ed271a'
-player2_colour_light='#c96b65'
-Green_colour='#5eab20'
+dot_color='#5eab20'
+player1_color='#158abd'
+player1_color_light='#7fbad4'
+player2_color='#ed271a'
+player2_color_light='#c96b65'
+Green_color='#5eab20'
 dot_width=0.25*size_of_board/number_of_dots
 edge_width=0.1*size_of_board/number_of_dots
 distance_between_dots=size_of_board/(number_of_dots)
@@ -223,3 +223,7 @@ class Dots_and_Boxes():
             self.canvas.delete("all")
             self.play_again()
             self.reset_board = False
+
+game_instance = Dots_and_Boxes()
+game_instance.mainloop()
+
