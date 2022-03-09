84e272f434f6ef863c075c3112c88ef9f996d9f3
shyam <vellurushyam7@gmail.com>
2022-03-09 12:21:57 +0530

    creating_Oval&Cross_canvas

--- tictac.py
+++ tictac.py
@@ -32,8 +32,53 @@ class Tic_Tac_Toe():
            for i in range(2):
               self.canvas.create_line((i + 1) * size_of_board / 3, 0, (i + 1) * size_of_board / 3, size_of_board)
             
+           for i in range(2):
+              self.canvas.create_line(0,(i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
+       
+       def play_again(self):
+            self.initialize_board()
+            self.player_X_starts = not self.player_X_starts
+            self.player_X_turns = self.player_X_starts
+            self.board_status = np.zeros(shape=(3, 3))
 
 
+ 
+       def draw_O(self, logical_position):
+            logical_position = np.array(logical_position)
+            grid_position = self.convert_logical_to_grid_position(logical_position)
+            self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                    grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
+                                    outline=symbol_O_color)
+
+
+       def draw_X(self, logical_position):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
 
 
 
